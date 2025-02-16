49baec16b7d175bc532b6d5ff805f4a03b30d219
songsong <kysong794@naver.com>
2025-02-17 00:52:06 +0900

    up/down game

--- number-guess-game/main.js
+++ number-guess-game/main.js
@@ -12,24 +12,72 @@ let computerNum = 0;
 let playbutton = document.getElementById("play-Button");
 let userInput = document.getElementById("user-input");
 let resultArea = document.getElementById("result-area");
+let resetButton = document.getElementById("reset-button");
+let chanceArea = document.getElementById("chance-area");
+let correct = document.getElementById("correct");
+let chances = 3;
+let gameOver = false;
+let history=[];
 
 playbutton.addEventListener("click", play);
+resetButton.addEventListener("click", reset);
+userInput.addEventListener("focus", function(){
+    userInput.value="";
+});
 
 function pickRandomNum(){
     computerNum = Math.floor(Math.random() * 100)+1;
-    console.log("정답", computerNum);
+    //console.log("정답", computerNum);
+    correct.textContent=`정답 : ${computerNum}`;
 }
 console.log(computerNum);
 function play(){
     let userValue = userInput.value;
+
+
+    if(userValue <1 || userValue>100){
+        resultArea.textContent="1과 100사이 숫자를 입력해 주세요.";
+        return;
+    }
+    if(history.includes(userValue)){
+        resultArea.textContent="이미 입력한 번호입니다.  다른 숫자를 입력해주세요";
+        return;
+    }
+    chances --;
+    chanceArea.textContent = `기회 : ${chances}번`;
+    console.log("chances", chances);
+
     if(userValue < computerNum){
         resultArea.textContent = "UP";
     } else if(userValue > computerNum){
         resultArea.textContent = "DOWN";
     } else{
         resultArea.textContent = "정답";
+        gameOver=true;
     }
-    console.log(userValue)
+    
+    history.push(userValue);
+    console.log(history);
+
+    if(chances < 1){
+        gameOver = true;
+    }
+    if(gameOver == true){
+        playbutton.disabled = true; 
+    }
+
+}
+
+function reset(){
+    //user input 창 깨끗하게 정리
+    userInput.value = "";
+    
+    //새로운번호 생성
+    pickRandomNum();   
+
+    resultArea.textContent="결과값이 여기 나옵니다"
+
+    playbutton.disabled = false; 
 }
 
 pickRandomNum();
