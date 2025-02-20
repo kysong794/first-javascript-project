31df2022d3f5d03397e4dc345e026e13930a9e34
songsong <kysong794@naver.com>
2025-02-21 01:05:11 +0900

    기능 추가 및 CSS 수정

--- to-do-list/main.js
+++ to-do-list/main.js
@@ -8,6 +8,12 @@
 // 진행중 끝남 탭을 누르면, 언더바가 이동한다
 // 끝남탭은, 끝난 아이템만, 진행중탭은 진행중인 아이템만
 //전체 탭을 누르면 다시 전체 아이템으로 돌아옴
+//엔터기능추가
+//메뉴언다바 탭에맞게 이동
+// 1. 할일 체크버튼 클릭에 따른 상태값 변화를 동적으로 리스트조정 진행중<->끝남
+// 1-1. 리스트 변경되면서 알림창 뛰우기
+// 2. 진행중 탭에서 등록시 리스트에도 보이게 조정
+// 2-1 끝남 탭에서 등록시 진행중탭으로 이동
 
 let taskInput = document.getElementById("task-input");
 let addButton = document.getElementById("add-button");
@@ -22,6 +28,20 @@ for(let i=1; i<tabs.length;i++){
         filter(event);
     });
 }
+//메뉴언더바 start
+let underLine = document.getElementById("under-line");
+let menus = document.querySelectorAll(".task-tabs div:not(#under-line)");
+window.addEventListener("load", () => {
+    let defaultMenu = document.getElementById("all");
+    setUnderline(defaultMenu);
+});
+menus.forEach(menu => menu.addEventListener("click", (e) => setUnderline(e.currentTarget)));
+
+function setUnderline(target) { 
+    underLine.style.left = target.offsetLeft + "px";
+    underLine.style.width = target.offsetWidth + "px";
+}
+//메뉴언더파 end
 
 //엔터기능 추가
 taskInput.addEventListener("keyup", function (event) {
@@ -41,7 +61,19 @@ function addTask(){
         taskContent:taskContent,
         isComplete:false
     }
-    taskList.push(task)
+    taskList.push(task);
+
+    //현재 선택된 탭이 "진행중"일때 진행중 리스트에도 추가
+    if(mode === "ongoing") {
+        filterList.push(task);
+    }
+
+    // 현재 선택된 탭이 "끝남"일때, 새로운 할일은 진행중으로 이동
+    if(mode ==="done") {
+        mode = "ongoing";
+        setUnderline(document.getElementById("ongoing"));
+        filterList = taskList.filter(task => !task.isComplete);
+    }
     
     //입력창 초기화
     taskInput.value = "";
@@ -82,12 +114,22 @@ function render() {
     document.getElementById("task-board").innerHTML = resultHTML;
 }
 
+//할일 체크 버튼(아이콘)
 function toggleComplete(id){
     for(let i=0; i<taskList.length; i++){
         if(taskList[i].id == id){
             taskList[i].isComplete=!taskList[i].isComplete;
             break;
         }
+    }
+
+    //진행중,끝남 각 탭에서 변경에 맞게 리스트에도 변화 
+    if (mode === "ongoing") {
+        alert('끝남으로 이동합니다');
+        filterList = taskList.filter(task => !task.isComplete); // 진행중만 남기기
+    } else if (mode === "done") {
+        alert('진행중으로 이동합니다');
+        filterList = taskList.filter(task => task.isComplete); // 끝난 할 일만 남기기
     }
     render();
 }
