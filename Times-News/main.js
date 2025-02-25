c6edeba9fdf72b10c0e0161c67e19f976daa6780
songsong <kysong794@naver.com>
2025-02-26 00:46:21 +0900

    test

---
+++ Times-News/main.js
@@ -0,0 +1,11 @@
+const API_KEY=`c41201efd9bd470f867b38d7dd4e53b3`;
+
+const getLatestNews = async() => {
+    const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+    const response = await fetch(url);
+    const data = await response.json();
+    let news = data.articles;
+    console.log("response", news)
+}
+
+getLatestNews();
