c95c0e940e1bbf79b6b5544e7b9921d0bba5e7c2
songsong <kysong794@naver.com>
2025-02-26 01:04:11 +0900

    url변경

--- Times-News/main.js
+++ Times-News/main.js
@@ -1,7 +1,8 @@
 const API_KEY=`c41201efd9bd470f867b38d7dd4e53b3`;
 
 const getLatestNews = async() => {
-    const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+    //const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+    const url = new URL(`https://study-website-be-bbb1539aa813.herokuapp.com/`);
     const response = await fetch(url);
     const data = await response.json();
     let news = data.articles;
