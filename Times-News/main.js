396a54ab72c5980685fb4f7c8a648a5bbecc0c8e
songsong <kysong794@naver.com>
2025-02-26 15:12:50 +0900

    url 재수정

--- Times-News/main.js
+++ Times-News/main.js
@@ -2,7 +2,7 @@ const API_KEY=`c41201efd9bd470f867b38d7dd4e53b3`;
 
 const getLatestNews = async() => {
     //const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
-    const url = new URL(`https://study-website-be-bbb1539aa813.herokuapp.com/`);
+    const url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr`);
     const response = await fetch(url);
     const data = await response.json();
     let news = data.articles;
